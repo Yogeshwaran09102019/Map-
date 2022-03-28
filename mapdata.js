2439bb7ff58f49826337ddde468fe9a90aab2284
Yogeshwaran S <75844322+Yogeshwaran09102019@users.noreply.github.com>
2022-03-28 09:57:45 +0530

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,206 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "550", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "red",
+    state_url:"",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "18",
+    location_type: "circle",
+    location_border: 2,
+    location_hover_border: 1,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar"
+    },
+    "2": {
+      name: "Andhra Pradesh"
+    },
+    "3": {
+      name: "Arunachal Pradesh"
+    },
+    "4": {
+      name: "Assam"
+    },
+    "5": {
+      name: "Bihar"
+    },
+    "6": {
+      name: "Chandigarh"
+    },
+    "7": {
+      name: "Chhattisgarh"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli"
+    },
+    "9": {
+      name: "Daman And Diu"
+    },
+    "10": {
+      name: "Delhi"
+    },
+    "11": {
+      name: "Goa"
+    },
+    "12": {
+      name: "Gujarat"
+    },
+    "13": {
+      name: "Haryana"
+    },
+    "14": {
+      name: "Himachal Pradesh"
+    },
+    "16": {
+      name: "Jharkhand"
+    },
+    "17": {
+      name: "Karnataka"
+    },
+    "18": {
+      name: "Kerala",
+      color:"#FC4F4F",
+    },
+    "19": {
+      name: "Lakshadweep"
+    },
+    "20": {
+      name: "Madhya Pradesh"
+    },
+    "21": {
+      name: "Maharashtra"
+    },
+    "22": {
+      name: "Manipur"
+    },
+    "23": {
+      name: "Meghalaya"
+    },
+    "24": {
+      name: "Mizoram"
+    },
+    "25": {
+      name: "Nagaland"
+    },
+    "26": {
+      name: "Orissa"
+    },
+    "27": {
+      name: "Puducherry"
+    },
+    "28": {
+      name: "Punjab"
+    },
+    "29": {
+      name: "Rajasthan"
+    },
+    "30": {
+      name: "Sikkim"
+    },
+    "31": {
+      name: "Tamil Nadu"
+    },
+    "32": {
+      name: "Tripura"
+    },
+    "33": {
+      name: "Uttar Pradesh"
+    },
+    "34": {
+      name: "Uttaranchal"
+    },
+    "35": {
+      name: "West Bengal"
+    },
+    "36": {
+      name: "Jammu And Kashmir"
+    },
+    "37": {
+      name: "Telangana"
+    },
+    "38": {
+      name: "Ladakh"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "11.2588",
+      lng: "75.7804",
+      name: "Kozhikode",
+      description: "Flood Prone: Yes",
+      color:"#00FF00"
+    },
+    "1": {
+      lat: "9.931233",
+      lng: "76.267303",
+      name: "Kochi",
+      description: "Flood Prone: No",
+      color:"black"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
